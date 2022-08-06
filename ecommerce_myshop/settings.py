beea0fdaac0b0ebecb91f790f8a4656d1fec2fb6
vinit-hvt <vinit.tekwani@hexaviewtech.com>
2022-08-06 20:42:16 +0530

    Added Procfile and Updated Settings.py file

--- ecommerce_myshop/settings.py
+++ ecommerce_myshop/settings.py
@@ -131,6 +131,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- ecommerce_myshop/urls.py
+++ ecommerce_myshop/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('myshop/', include('LoginSignup.urls')),
     path('myshop/', include('myshop.urls')),
     path('myshop/', include('userinfo.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
