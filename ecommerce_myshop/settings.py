c83d7454bbf64ce0cc142ab1491e8290dce59f3d
vinit-hvt <vinit.tekwani@hexaviewtech.com>
2022-08-14 21:02:51 +0530

    Merged dev into Master

--- ecommerce_myshop/settings.py
+++ ecommerce_myshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--0-_^9x=aq*piao1m08kzvjl6xrjkm83*v!ef1ay9un55k1z(n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['its-myshop.herokuapp.com']
 
 
 # Application definition
@@ -117,10 +117,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,6 +133,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Add these new lines
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
--- ecommerce_myshop/urls.py
+++ ecommerce_myshop/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myshop/', include('LoginSignup.urls')),
     path('myshop/', include('myshop.urls')),
     path('myshop/', include('userinfo.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root' : settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root' : settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
